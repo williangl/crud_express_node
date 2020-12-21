98d3a1d45548bcf48da13d940c1006c0155729e0
Willian Lopes <williangldzn@gmail.com>
2020-12-21 18:29:21 -0300

    Create BookAdress model

---
+++ model/models.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const addressSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  place: {
+    type: String,
+    required: true,
+  },
+});
+
+const Address = mongoose.model('Address', addressSchema);
+
+module.exports = Address;
